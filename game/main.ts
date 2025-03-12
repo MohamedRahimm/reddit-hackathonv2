5a4f809077b36211c0ca6c603fb3f9285f21ae07
Mohamed <mohamedsrahim2005@gmail.com>
2025-03-11 23:16:42 -0400

    minor fixes

--- game/main.ts
+++ game/main.ts
@@ -1,1 +1,4 @@
-console.log("test")
+console.log("hi")
+const elem = document.createElement("p")
+document.body.appendChild(elem)
+elem.innerText="jfoejf"
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
-import { defineConfig } from "vite";
-// @ts-expect-error Too lazy
 import path from "path";
-// https://github.com/tailwindlabs/tailwindcss/issues/16751
-// @ts-expect-error Tailwind and Vite are having some problems ATM
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [],
-  root: path.join(__dirname, "game"), // Point to your app directory
+  root: path.join(__dirname, "game"),
   build: {
-    outDir: path.join(__dirname, "webroot"), // Specify your desired output directory
-    emptyOutDir: true, // Clean the output directory before each build
-    copyPublicDir: true, // Copies over assets
-    sourcemap: true, // Enable sourcemaps
+    outDir: path.join(__dirname, "webroot"),
+    emptyOutDir: true,
+    copyPublicDir: true,
+    sourcemap: true,
   },
 });
